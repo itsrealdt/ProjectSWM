9f3651d1f1ff02a9bdde72d80318b80719e04064
David Fiorin <david94lozoo@gmail.com>
2017-10-05 16:52:44 +0200

    - Inserita scena Animation UI

---
+++ Assets/Script/MovePanel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePanel : MonoBehaviour
+{
+    private RectTransform initialPos;
+
+    private void Awake()
+    {
+        initialPos = this.GetComponent<RectTransform>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            StartCoroutine(MovePanelCO());   
+        }
+    }
+
+    private IEnumerator MovePanelCO()
+    {
+        while (true)
+        {
+            this.GetComponent<RectTransform>().position -= new Vector3(100, 0, 0);
+            yield return null;
+        }
+    }
+}
