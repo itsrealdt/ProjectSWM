7dffbd1411c899ad290ba0bc6950f55dfb2e3cb1
David Fiorin <david94lozoo@gmail.com>
2017-10-16 12:17:06 +0200

    - Inserita scena Animation UI e script MovePanel in Unity 2017.2 e fixato bug script

--- Assets/Script/MovePanel.cs
+++ Assets/Script/MovePanel.cs
@@ -7,22 +7,28 @@ public class MovePanel : MonoBehaviour
     private Vector3 initialPos = Vector3.zero;
     private Vector3 originalPos;
     public float speedMove = 25f;
+    public bool isMoving;
 
     public void MovePanelMeth(GameObject _go)
     {
-        // Prendo qui la posizione originale dell'oggetto in modo da porterlo poi
-        // far tornare in posizione con ReturnInPosition
-        originalPos = _go.GetComponent<RectTransform>().localPosition;
-        StartCoroutine(MovePanelCO(_go));
+        if (!isMoving)
+        {
+            // Prendo qui la posizione originale dell'oggetto in modo da porterlo poi
+            // far tornare in posizione con ReturnInPosition solo se non si sta muovendo
+            originalPos = _go.GetComponent<RectTransform>().localPosition;
+            StartCoroutine(MovePanelCO(_go));
+        }
     }
 
     private IEnumerator MovePanelCO(GameObject _go)
     {
         while (_go.GetComponent<RectTransform>().localPosition != initialPos)
         {
+            isMoving = true;
             _go.GetComponent<RectTransform>().localPosition = Vector3.MoveTowards(_go.GetComponent<RectTransform>().localPosition, initialPos, speedMove);
             yield return null;
         }
+        isMoving = false;
     }
 
     public void ReturnInPosition(GameObject _go)
@@ -34,8 +40,10 @@ public class MovePanel : MonoBehaviour
     {
         while (_go.GetComponent<RectTransform>().localPosition != originalPos)
         {
+            isMoving = true;
             _go.GetComponent<RectTransform>().localPosition = Vector3.MoveTowards(_go.GetComponent<RectTransform>().localPosition, originalPos, speedMove);
             yield return null;
         }
+        isMoving = false;
     }
 }
